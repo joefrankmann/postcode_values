8edbea4e04604fca5eb8f544b9c8d06023814de6
Joe Frankmann <franmann.joao@gmail.com>
2021-03-25 11:42:09 +0000

    update views.py

--- postcodevalue/postcodes/api/views.py
+++ postcodevalue/postcodes/api/views.py
@@ -62,17 +62,6 @@ class NexusListCreateAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# class NexusDetailsAPIView(APIView):
-
-#     def get_object(self, outcode_code):
-#         outcode = get_object_or_404(NexusOutcode, outcode_code=outcode_code)
-#         return outcode
-    
-#     def get(self, request, outcode_code):
-#         outcode = self.get_object(outcode_code)
-#         serializer = NexusSerializer(outcode)
-#         return Response(serializer.data)
-
 class NexusDetailsAPIView(generics.RetrieveAPIView):
     serializer_class = NexusSerializer
     queryset = NexusOutcode.objects.all()
@@ -86,17 +75,6 @@ class NexusDetailsAPIView(generics.RetrieveAPIView):
         serializer = NexusSerializer(outcode)
         return Response(serializer.data)
 
-    # def put(self, request, outcode_code):
-    #     outcode = self.get_object(outcode_code)
-    #     serializer = NexusSerializer(outcode, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # def delete(self, request, outcode_code):
-    #     outcode = self.get_object(outcode_code)
-    #     outcode.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
